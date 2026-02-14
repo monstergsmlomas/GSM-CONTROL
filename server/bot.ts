436baa7ef46818be3ee05c460a089fe5fbea2e46
monstergsmlomas <monstergsmlomas@gmail.com>
2026-02-14 11:05:30 -0300

    fix configuraciones y guardado

--- server/bot.ts
+++ server/bot.ts
@@ -20,7 +20,6 @@ export const initWhatsApp = () => {
                 '--disable-accelerated-2d-canvas',
                 '--no-first-run',
                 '--no-zygote',
-                '--single-process',
                 '--disable-gpu'
             ],
             timeout: 60000
